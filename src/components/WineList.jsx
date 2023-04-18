a7351e81c9fec965a2a7199603155c1b2d51e64d
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-04-18 11:11:48 -0400

    ADDED useEffect && buttons

--- src/components/WineList.jsx
+++ src/components/WineList.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import WineCard from "./WineCard"
 
 export default function WineList () {
 
     const [theWines, setTheWines] = useState()
+    const [color, setColor] =useState ('reds')
 
     const getWines = () => {
-    fetch('https://api.sampleapis.com/wines/reds')
+    fetch(`https://api.sampleapis.com/wines/${color}`)
         .then(response => response.json())
         .then(data => setTheWines(data))
         .catch(alert)
     }
 
+    useEffect(() => {
+        getWines()
+    }, [color])
+
     return ( 
-        <section className="wine-list"> 
-        {(!theWines)
-            ? <button onClick={getWines}>Get Wine List</button>
-            : theWines.map(wine => (
-                <WineCard key= {wine.id} wine={wine}/>
-            ))
-        }
-        </section>
+        <>
+            <div className="buttons">
+                <button onClick={() => setColor('reds')}>Reds</button>
+                <button onClick={() => setColor('whites')}>Whites</button>
+                <button onClick={() => setColor('sparkling')}>Sparkling</button>
+                <button onClick={() => setColor('rose')}>Rose</button>
+
+            </div>
+            <section className="wine-list">
+                {(!theWines)
+                    ? <h2>Loading...</h2>
+                    : theWines.map(wine => (
+                        <WineCard key= {wine.id} wine={wine}/>
+                    ))
+                }
+            </section>
+        </>
     )
 }
